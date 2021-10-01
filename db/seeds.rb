46733b3c765bffbe12229eb4273dc2658cb72c30
Kris Hopper <hopperdavis@gmail.com>
2021-10-01 10:43:35 -0700

    WIP: testing API calls in Postman, researching seeding database with one API call

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# Figure out how to store API call in seed
